6f90114f004fe66b13c719f4d1cf0cd487bda7aa
Rezart B <rezartbe@gmail.com>
2023-01-29 21:28:10 -0500

    this is practice commit

--- src/main/java/io/yoll/Main.java
+++ src/main/java/io/yoll/Main.java
@@ -3,5 +3,7 @@ package io.yoll;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        System.out.println("this is my first change");
     }
 }
